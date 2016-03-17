129398baa9dbfad9991d521c31cc1826259bed68
David <davesbrains@gmail.com>
2016-03-17 02:43:24 +0000

    tests for jobs get and refactored

---
+++ test/jobs-data-spec.js
@@ -0,0 +1,26 @@
+var expect = require("chai").expect;
+var mongoose = require('mongoose');
+var jobModel = require('../models/Job');
+var Promise = require("bluebird");
+
+
+function resetJobs(){
+    return new Promise( function(resolve, reject){
+        mongoose.connection.collections['jobs'].drop(resolve, reject);
+    });
+}
+
+describe("get jobs", function(){
+   it("should never be empty since jobs are seeded", function(done){
+       mongoose.connect('mongodb://localhost/jobfinder', function(){
+           resetJobs()
+           .then(jobModel.seedJobs())
+           .then(function(){
+                    mongoose.model('Job').find({}).exec(function(error, jobsList){
+                        expect(jobsList.length).to.be.at.least(1); 
+                        done();
+                    })
+           });
+       });
+    });
+});
